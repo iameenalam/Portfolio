926058f00ea5e306fd2c5ff1d07e08dc4ff4e907
Ameen Alam~ <ameenalam98@gmail.com>
2024-08-15 20:47:00 +0500

    created services component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Services from './components/Services/Services';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Services />
     </main>
     </>
   )
