47f406feca859000074b8ae74d2b3f107adf0f34
Ameen Alam~ <ameenalam98@gmail.com>
2024-08-18 02:53:19 +0500

    a few adjustments and changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,20 +27,8 @@ const Header = () => {
             </li>
 
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
-                <i className="uil uil-file-alt nav__icon"></i> Skills
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#services" className="nav__link">
-                <i className="uil uil-briefcase-alt nav__icon"></i> Services
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
-                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              <a href="#projects" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Projects
               </a>
             </li>
 
--- src/components/Home/Data.jsx
+++ src/components/Home/Data.jsx
@@ -57,7 +57,7 @@ const Data = () => {
       </h1>
       <h3 className="home__subtitle">Web Developer</h3>
       <p className="home__description">
-        I'm web developer based in Pakistan, and I'm very passionate and
+        I'm a web developer based in Pakistan, and I'm very passionate and
         dedicated to my work.
       </p>
 
