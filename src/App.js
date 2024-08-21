ea3685b1abbfa7c78d571e80a0fb820cf0d36f3a
Ameen Alam~ <ameenalam98@gmail.com>
2024-08-21 14:34:52 +0500

    added Scroll-to-Top feature

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Skills from './components/Skills/Skills';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
 // import Services from './components/Services/Services';
-import Qualification from './components/Qualification/Qualification';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
+import Qualification from "./components/Qualification/Qualification";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
 
 const App = () => {
   return (
     <>
-    <Header />
+      <Header />
 
-    <main className='home'>
-      <Home />
-      <About />
-      <Skills />
-      {/* <Services /> */}
-      <Qualification />
-      <Projects />
-      <Contact />
+      <main className="home">
+        <Home />
+        <About />
+        <Skills />
+        {/* <Services /> */}
+        <Qualification />
+        <Projects />
+        <Contact />
+      </main>
+
       <Footer />
-    </main>
+      <ScrollUp />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
